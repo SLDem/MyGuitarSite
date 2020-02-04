a824f58186842a023a91812283d6c5ed81d30413
Yaroslav <slavko.dem@gmail.com>
2020-02-04 14:45:14 +0200

    odified main.py

--- main.py
+++ main.py
@@ -31,36 +31,6 @@ class users(db.Model):
     """
 
 
-class Config(object):
-    LOG_TO_STDOUT = os.environ.get('LOG_TO_STDOUT')
-
-
-def create_app(config_class=Config):
-    # ...
-    if not app.debug and not app.testing:
-        # ...
-
-        if app.config['LOG_TO_STDOUT']:
-            stream_handler = logging.StreamHandler()
-            stream_handler.setLevel(logging.INFO)
-            app.logger.addHandler(stream_handler)
-        else:
-            if not os.path.exists('logs'):
-                os.mkdir('logs')
-            file_handler = RotatingFileHandler('logs/microblog.log',
-                                               maxBytes=10240, backupCount=10)
-            file_handler.setFormatter(logging.Formatter(
-                '%(asctime)s %(levelname)s: %(message)s '
-                '[in %(pathname)s:%(lineno)d]'))
-            file_handler.setLevel(logging.INFO)
-            app.logger.addHandler(file_handler)
-
-        app.logger.setLevel(logging.INFO)
-        app.logger.info('Microblog startup')
-
-    return app
-
-
 @app.route('/index')
 def index():
     return render_template('index.html')
